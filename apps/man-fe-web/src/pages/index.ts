0731952ab4e55df096f50a0314289588b2641cf0
junman9595@gmail.com <jmc@marpple.com>
2025-09-10 18:29:59 +0900

    chore: 일부 설정 추가

--- apps/man-fe-web/src/pages/index.ts
+++ apps/man-fe-web/src/pages/index.ts
@@ -1,7 +1,7 @@
 import type { GetServerSideProps } from "@man/fe-kit/rune/utils/type";
 import { View } from "@man/fe-kit/rune/View";
-import css from "@man/fe-web/src/pages/Main.module.scss";
 import { html } from "rune-ts";
+import css from "./Main.module.scss";
 
 export const getServerSideProps: GetServerSideProps<{ title: string }> = (
   _context
---
+++ apps/man-fe-web/src/types/scss.d.ts
@@ -0,0 +1,24 @@
+declare module "*.module.scss" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.scss" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.sass" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
